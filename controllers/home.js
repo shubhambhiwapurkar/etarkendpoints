6cb7220b3f40506c94fec53b60bd1c8113becffb
shubham <shubhambhiwapurkar@gmail.com>
2021-09-02 23:39:30 +0530

    home

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,15 @@
-const auth = require("../controllers/auth");
+exports.verifyToken = (req, res, next) => {
+    const token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
 
-app.post("/home", auth, (req, res) => {
-    res.status(200).send("Welcome 🙌 ");
-});
+    if (!token) {
+        return res.status(403).send("A token is required for authentication");
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET);
+        req.user = decoded;
+    } catch (err) {
+        return res.status(401).send("Invalid Token");
+    }
+    return next();
+};
--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,5 @@
-const express = require("express")
-const router = express.Router()
+const auth = require("../controllers/home");
 
-const { isAuthenticated, isSignedIn } = require("../controllers/auth")
-const { getUserById } = require("../controllers/user")
-
-
-//params
-router.param("userID", getUserById);
-
-router.post("/category/create/:userId", isSignedIn, isAuthenticated);
-
-module.exports = router;
+app.post("/home", auth, (req, res) => {
+    res.status(200).send("Welcome 🙌 ");
+});
